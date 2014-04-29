2fceba753042729f2a39c1a2e38fe3566321500c
Yung Hwa Kwon <yung.kwon@damncarousel.com>
2014-04-28 23:35:32 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,124 @@
+
+/*
+ * dependencies
+ */
+
+var moment = require('moment');
+var util = require('util');
+
+
+/*
+ * days in english
+ */
+
+var days = [
+  'sunday',
+  'monday',
+  'tuesday',
+  'wednesday',
+  'thursday',
+  'friday',
+  'saturday'
+];
+
+
+/*
+ * Calendar
+ *
+ * @param {Date} date
+ * @constructor
+ */
+
+var Calendr = module.exports = function(date) {
+  var today = this.date = moment(date);
+  var day = this.day = today.date();
+  var month = this.month = today.month()+1;
+  var year = this.year = today.year();
+  var numofdays = this.numofdays = today.daysInMonth();
+
+  this.monthstartson = moment(year+'-'+month, 'YYYY-MM').day();
+  this.monthendson = moment(year+'-'+month+'-'+numofdays, 'YYYY-MM-DD').day();
+};
+
+
+/*
+ * array grid of calendar month
+ *
+ * @return {Array}
+ * @api public
+ */
+
+Calendr.prototype.__defineGetter__('grid', function() {
+  return this.slice();
+});
+
+
+/*
+ * slices the calendar days up into weeks
+ *
+ * @return {Array}
+ * @api public
+ */
+
+Calendr.prototype.slice = function() {
+  var days = calendardays.call(this);
+  prepad(days, this.monthstartson);
+
+  var numofweeks = Math.ceil(days.length / 7);
+  var weeks = Array.apply(null, Array(numofweeks)).map(function(o, i) {
+    var b = i*7;
+    var e = 7*(i+1);
+    return days.slice(b, e);
+  });
+
+  fill(weeks[numofweeks-1]);
+
+  return weeks;
+};
+
+
+/*
+ * create an array of calendar days
+ *
+ * @return {Array}
+ * @api private
+ */
+
+function calendardays() {
+  var days = Array.apply(null, Array(this.numofdays));
+  return days.map(function(d, i) {
+    return i+1;
+  });
+}
+
+
+/*
+ * pad days so 1st day starts on same index of day of the week
+ *
+ * @param {Array} days
+ * @param {Number} index
+ * @return {Array}
+ * @api private
+ */
+
+function prepad(days, index) {
+  var i = 0;
+  for(; i<index; i++) {
+    days.splice(0, 0, null);
+  }
+}
+
+
+/*
+ * pad the last week to be a full 7 length array
+ *
+ * @param {Array} week
+ * @api private
+ */
+
+function fill(week) {
+  for(var i=week.length; i<7; i++) {
+    week.push(null);
+  }
+}
+
