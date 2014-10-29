f28044ddce395da71c42e2ab8f11a54c3a6ebd68
Yung Hwa Kwon <yung.kwon@damncarousel.com>
2014-10-29 18:57:43 -0400

    Refactor event

--- event.js
+++ event.js
@@ -1,173 +1,245 @@
-;(function(window) {
-  var moment, Event;
+/* jshint node: true */
 
-  function check(k) {
-    if (!(k in window)) {
-      throw new Error(k+" is required");
-    }
+var moment = require("moment");
+
+/**
+ * expose Event
+ */
+
+module.exports = Event;
+
+/**
+ * parseDate converts to Date
+ *
+ * @param {String|Number|Date} v
+ * @return {Moment}
+ * @api private
+ */
+
+var parseDate = function(v) {
+  if (!!!v) {
+    return;
   }
 
-  /**
-   * expose Event
-   */
+  return moment(v);
+}
 
-  if (window) {
-    check("moment");
-    
-    moment = window.moment;
-    window.Event = Event;
-  } else {
-    moment = require("moment");
-    module.exports = Event;
+/**
+ * defaultConfig is the default mapping table
+ */
+
+var defaultConfig = {
+  id:           "id",
+  name:         "name",
+  description:  "description",
+  starts:       "starts",
+  ends:         "ends",
+  repeats:      "repeats",
+  repeatsOn:    "repeats_on",
+  repeatEndsOn: "repeat_ends_on",
+  repeatTimes:  "repeat_times"
+}
+
+/**
+ * extend extends the config object with any missing properties from 
+ * defaultConfig
+ *
+ * @param {Object} orig
+ * @return {Object}
+ * @api private
+ */
+
+function extend(config) {
+  var keys = Object.keys(defaultConfig);
+  var copy = {};
+  var i = 1;
+  var len = keys.length;
+  for(; i < len; i++) {
+    var k = keys[i];
+    var v = config[k];
+    copy[k] = !!v ? v : defaultConfig[k];
   }
 
-  /**
-   * parseDate converts to Date
-   *
-   * @param {String|Number|Date} v
-   * @return {Moment}
-   * @api private
-   */
+  return copy;
+}
 
-  var parseDate = function(v) {
-    if (!!!v) {
-      return;
-    }
- 
-    return moment(v);
+/**
+ * assign assigns properties from data based on config
+ *
+ * @param {Object} obj
+ * @param {Object} config
+ * @api private
+ */
+
+function assign(obj, config) {
+  var self = this;
+  var conf = !!config ? extend(config) : defaultConfig;
+  var keys = Object.keys(conf);
+  var i = 0;
+  var len = keys.length;
+  for(; i < len; i++) {
+    var k = keys[i];
+    var v = conf[k];
+    self[k] = obj[v];
+  }
+}
+
+/**
+* Event
+*
+* @param {Object} obj (should be plain old object, like a JSON parse)
+* @param {Object} config
+* @constructor
+* @api public
+*/
+
+function Event(obj, config) {
+  assign.call(this, (obj || {}), config);
+
+  this.starts = parseDate(this.starts);
+  this.ends = parseDate(this.ends);
+  this.repeatEndsOn = parseDate(this.repeatEndsOn);
+  if (!!!this.starts || !!!this.ends) {
+    throw new Error("must have a starts and ends date") ;
+  }
+}
+
+/**
+ * duration returns the duration of an event
+ *
+ * @return {Duration}
+ * @api public
+ */
+
+Event.prototype.duration = function() {
+  return new Duration(this.starts, this.ends);
+};
+
+/**
+ * days returns a collection of EventDays in which the event spans across
+ *
+ * @return {[EventDay]}
+ * @api public
+ */
+
+Event.prototype.days = function() {
+  var d = this.duration();
+  var self = this;
+  var days = [];
+  var i = 0;
+  var len = d.length();
+  for(; i < len; i++) {
+    days.push(new EventDay(self, i));
+  }
+  return days;
+};
+
+/**
+ * EventDay is a wrapper around Event to describe the event on a particular day
+ *
+ * @param {Event} event
+ * @param {Number} i (index of the day in the span of the event)
+ * @constructor
+ * @api private
+ */
+
+function EventDay(event, i) {
+  this.event = event;
+  this.i = i || 0;
+
+  var date = this.date = moment(this.event.starts);
+  date.startOf("day");
+  date.add(this.i, "days");
+};
+
+/**
+ * start returns the start date of the event for the given day
+ * 
+ * @return {Moment}
+ * @api public
+ */
+
+EventDay.prototype.starts = function() {
+  var starts = moment(this.event.starts);
+  starts.startOf("day");
+  if (sameDay(this.date, starts)) {
+    return this.event.starts;
   }
 
-  /**
-   * defaultConfig is the default mapping table
-   */
+  var sod = moment(this.date);
+  sod.startOf("day");
+  return sod;
+}
 
-  var defaultConfig = {
-    name:         "name",
-    description:  "description",
-    starts:       "starts",
-    ends:         "ends",
-    repeats:      "repeats",
-    repeatsOn:    "repeats_on",
-    repeatEndsOn: "repeat_ends_on",
-    repeatTimes:  "repeat_times"
+/**
+ * ends returns the end date of the event for the given day
+ * 
+ * @return {Moment}
+ * @api public
+ */
+
+EventDay.prototype.ends = function() {
+  var ends = moment(this.event.ends);
+  ends.startOf("day");
+  if (sameDay(this.date, ends)) {
+    return this.event.ends;
   }
 
-  /**
-   * extend extends the config object with any missing properties from 
-   * defaultConfig
-   *
-   * @param {Object} orig
-   * @return {Object}
-   * @api private
-   */
+  var eod = moment(this.date);
+  eod.endOf("day");
+  return eod;
+}
 
-  function extend(config) {
-    var keys = Object.keys(defaultConfig);
-    var copy = {};
-    var i = 1;
-    var len = keys.length;
-    for(; i < len; i++) {
-      var k = keys[i];
-      var v = config[k];
-      copy[k] = !!v ? v : defaultConfig[k];
-    }
+/**
+ * same day compares a pair of dates by their int representations
+ * 
+ * @param {Moment} a 
+ * @param {Moment} b 
+ * @return {Bool}
+ * @api private
+ */
 
-    return copy;
-  }
+function sameDay(a, b) {
+  return a.valueOf() === b.valueOf(); 
+}
 
-  /**
-   * assign assigns properties from data based on config
-   *
-   * @param {Object} obj
-   * @param {Object} config
-   * @api private
-   */
+/**
+ * Duration
+ *
+ * @param {Date} starts
+ * @param {Date} ends
+ * @constructor
+ * @api private
+ */
 
-  function assign(obj, config) {
-    var self = this;
-    var conf = !!config ? extend(config) : defaultConfig;
-    var keys = Object.keys(conf);
-    var i = 0;
-    var len = keys.length;
-    for(; i < len; i++) {
-      var k = keys[i];
-      var v = conf[k];
-      self[k] = obj[v];
-    }
-  }
+function Duration(starts, ends) {
+  this.starts = moment(starts);
+  this.ends = moment(ends);
+}
 
- /**
-  * Event
-  *
-  * @param {Object} obj (should be plain old object, like a JSON parse)
-  * @param {Object} config
-  * @constructor
-  */
+/**
+ * ms chart
+ */
 
-  function Event(obj, config) {
-    assign.call(this, (obj || {}), config);
+var ms_in_sec = 1000;
+var ms_in_min = 60 * ms_in_sec;
+var ms_in_hr = 60 * ms_in_min;
+var ms_in_day = 24 * ms_in_hr;
 
-    this.starts = parseDate(this.starts);
-    this.ends = parseDate(this.ends);
-    this.repeatEndsOn = parseDate(this.repeatEndsOn);
-    if (!!!this.starts) {
-      throw new Error("must have a starts date") ;
-    }
-  }
+/**
+ * length returns the number of days an event spans in relation to a calendar
+ * grid, this is not a 24 hour representation of length
+ *
+ * @return {Number}
+ * @api public
+ */
 
-  /**
-   * duration returns the duration of an event
-   *
-   * @return {Duration}
-   * @api public
-   */
+Duration.prototype.length = function() {
+  var a = this.starts;
+  var b = this.ends;
+  a.hours(0);
+  b.hours(0);
 
-  Event.prototype.duration = function() {
-    return new Duration(this.starts, this.ends);
-  };
-
-
-  /**
-   * Duration
-   *
-   * @param {Date} starts
-   * @param {Date} ends
-   * @constructor
-   */
-
-  function Duration(starts, ends) {
-    this.starts = starts;
-    this.ends = ends;
-  }
-
-  /**
-   * ms chart
-   */
-
-  var ms_in_sec = 1000;
-  var ms_in_min = 60 * ms_in_sec;
-  var ms_in_hr = 60 * ms_in_min;
-  var ms_in_day = 24 * ms_in_hr;
-  
-  /**
-   * length returns the number of days an event spans in relation to a calendar
-   * grid, this is not a 24 hour representation of length
-   *
-   * @return {Number}
-   * @api public
-   */
-
-  Duration.prototype.length = function() {
-    if (!!!this.ends) {
-      return 1;
-    }
-
-    var a = this.starts;
-    var b = this.ends;
-    a.hours(0);
-    b.hours(0);
-
-    var offset = 1; // always offset by 1 to account for the day of
-    return Math.ceil((b.valueOf() - a.valueOf()) / ms_in_day) + offset; 
-  };
-})(("undefined" === typeof module && "undefined" === typeof exports) ? window : null);
+  var offset = 1; // always offset by 1 to account for the day of
+  return Math.ceil((b.valueOf() - a.valueOf()) / ms_in_day) + offset; 
+};
--- test/event.js
+++ test/event.js
@@ -2,10 +2,11 @@
 
 var assert = require("chai").assert;
 var Event = require("../event");
+var Day = require("../day");
 
 describe('Event', function() {
   it("returns the length of an event", function() {
-    evt = new Event({
+    var evt = new Event({
       starts: new Date(2014, 0, 1, 0, 30, 55),
       ends: new Date(2014, 0, 4, 20, 12, 25)
     });
@@ -15,18 +16,9 @@ describe('Event', function() {
   });
 
   it("returns 1 if the start and end are on the same day", function() {
-    evt = new Event({
+    var evt = new Event({
       starts: new Date(2014, 0, 1, 0, 30, 55),
       ends: new Date(2014, 0, 1, 20, 12, 25)
-    });
-    d = evt.duration();
-    
-    assert.equal(d.length(), 1);
-  });
-
-  it("returns 1 if the end date is blank", function() {
-    evt = new Event({
-      starts: new Date(2014, 0, 1, 0, 30, 55)
     });
     d = evt.duration();
     
@@ -36,7 +28,7 @@ describe('Event', function() {
   it("throws if there is no start time", function() {
     assert.throw(function() {
       new Event();
-    }, /must have a starts date/);
+    }, /must have a starts and ends date/);
   });
 
   it("can map based on a configuration", function() {
@@ -57,7 +49,7 @@ describe('Event', function() {
       noTimes: 3,
     };
 
-    evt = new Event(data, config);
+    var evt = new Event(data, config);
     assert.equal(evt.starts.valueOf(), data.startson.getTime());
     assert.equal(evt.ends.valueOf(), data.endson.getTime());
     assert.equal(evt.repeats, data.repeats);
@@ -65,4 +57,38 @@ describe('Event', function() {
     assert.equal(evt.repeatEndsOn.valueOf(), data.repeatEndson.getTime());
     assert.equal(evt.repeatTimes, data.noTimes);
   });
+
+  it("returns a collection of days in which an event spans", function() {
+    var evt = new Event({
+      starts: new Date(2014, 0, 1, 0, 30, 55),
+      ends: new Date(2014, 0, 4, 14, 10, 21),
+    });
+
+    var days = evt.days();
+    assert.lengthOf(days, 4);
+
+    var table = [
+      {s: new Date(2014, 0, 1, 0, 30, 55), e: new Date(2014, 0, 1, 23, 59, 59, 999)},
+      {s: new Date(2014, 0, 2, 0, 0, 0), e: new Date(2014, 0, 2, 23, 59, 59, 999)},
+      {s: new Date(2014, 0, 3, 0, 0, 0), e: new Date(2014, 0, 3, 23, 59, 59, 999)},
+      {s: new Date(2014, 0, 4, 0, 0, 0), e: new Date(2014, 0, 4, 14, 10, 21)},
+    ];
+    var i = 0;
+    var len = table.length;
+    for(; i < len; i++) {
+      var d = days[i];
+      var v = table[i];
+      assert.equal(d.starts().valueOf(), v.s.getTime());
+      assert.equal(d.ends().valueOf(), v.e.getTime());
+    }
+  });
 });
+
+// var cal = new Calendr(2014, 0);
+// var []events = evt.apply(cal);
+
+// for _, v := range []events {
+//   for _, d := v.days {
+//     // look up day in cal and append
+//   } 
+// }
